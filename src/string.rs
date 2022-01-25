f2fac82ff20fce952e65c214ebfc1d2b3998f7d8
David Tolnay <dtolnay@gmail.com>
2022-01-25 01:00:16 -0800

    Add impls for decoding tuple type to string

--- src/string.rs
+++ src/string.rs
@@ -42,3 +42,78 @@ unsafe impl<const CH: char> RetrieveString for alphabet::four::char<CH> {
         (CH as u32 & 0x3F) as u8 | TAG_CONT,
     ];
 }
+
+unsafe impl RetrieveString for () {
+    type Type = ();
+    const BYTES: Self::Type = ();
+}
+
+#[repr(C)]
+struct Concat2<A, B>(A, B);
+
+unsafe impl<A, B> RetrieveString for (A, B)
+where
+    A: RetrieveString,
+    B: RetrieveString,
+{
+    type Type = Concat2<A::Type, B::Type>;
+    const BYTES: Self::Type = Concat2(A::BYTES, B::BYTES);
+}
+
+#[repr(C)]
+struct Concat3<A, B, C>(A, B, C);
+
+unsafe impl<A, B, C> RetrieveString for (A, B, C)
+where
+    A: RetrieveString,
+    B: RetrieveString,
+    C: RetrieveString,
+{
+    type Type = Concat3<A::Type, B::Type, C::Type>;
+    const BYTES: Self::Type = Concat3(A::BYTES, B::BYTES, C::BYTES);
+}
+
+#[repr(C)]
+struct Concat4<A, B, C, D>(A, B, C, D);
+
+unsafe impl<A, B, C, D> RetrieveString for (A, B, C, D)
+where
+    A: RetrieveString,
+    B: RetrieveString,
+    C: RetrieveString,
+    D: RetrieveString,
+{
+    type Type = Concat4<A::Type, B::Type, C::Type, D::Type>;
+    const BYTES: Self::Type = Concat4(A::BYTES, B::BYTES, C::BYTES, D::BYTES);
+}
+
+#[repr(C)]
+struct Concat5<A, B, C, D, E>(A, B, C, D, E);
+
+unsafe impl<A, B, C, D, E> RetrieveString for (A, B, C, D, E)
+where
+    A: RetrieveString,
+    B: RetrieveString,
+    C: RetrieveString,
+    D: RetrieveString,
+    E: RetrieveString,
+{
+    type Type = Concat5<A::Type, B::Type, C::Type, D::Type, E::Type>;
+    const BYTES: Self::Type = Concat5(A::BYTES, B::BYTES, C::BYTES, D::BYTES, E::BYTES);
+}
+
+#[repr(C)]
+struct Concat6<A, B, C, D, E, F>(A, B, C, D, E, F);
+
+unsafe impl<A, B, C, D, E, F> RetrieveString for (A, B, C, D, E, F)
+where
+    A: RetrieveString,
+    B: RetrieveString,
+    C: RetrieveString,
+    D: RetrieveString,
+    E: RetrieveString,
+    F: RetrieveString,
+{
+    type Type = Concat6<A::Type, B::Type, C::Type, D::Type, E::Type, F::Type>;
+    const BYTES: Self::Type = Concat6(A::BYTES, B::BYTES, C::BYTES, D::BYTES, E::BYTES, F::BYTES);
+}
