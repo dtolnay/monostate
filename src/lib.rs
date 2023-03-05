94ff13c44dae04517371248e4e862beab1d07d3c
David Tolnay <dtolnay@gmail.com>
2023-03-05 15:34:38 -0800

    Release 0.1.4

--- src/lib.rs
+++ src/lib.rs
@@ -49,7 +49,7 @@
 //! ```
 
 #![no_std]
-#![doc(html_root_url = "https://docs.rs/monostate/0.1.3")]
+#![doc(html_root_url = "https://docs.rs/monostate/0.1.4")]
 #![allow(non_camel_case_types, non_upper_case_globals)]
 #![allow(
     clippy::borrow_as_ptr,
