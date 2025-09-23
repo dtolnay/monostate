f91963da53f76277111ba42d68e210cba1889e76
David Tolnay <dtolnay@gmail.com>
2025-09-23 08:44:29 -0700

    Add &'static str associated constant

--- src/string.rs
+++ src/string.rs
@@ -1,4 +1,7 @@
 use crate::alphabet::{self, len};
+use core::mem::ManuallyDrop;
+use core::slice;
+use core::str;
 
 // Equivalent to `pub struct MustBeStr<const str: &'static str>;` but using
 // the type encoding described in impl/src/lib.rs to avoid depending on
@@ -16,6 +19,7 @@ impl<T> ConstStr for T where T: Sealed {}
 pub trait Sealed {
     type Type: 'static;
     const BYTES: Self::Type;
+    const VALUE: &'static str;
 }
 
 impl<T, const N: usize> Sealed for (len<N>, T)
@@ -24,6 +28,23 @@ where
 {
     type Type = T::Type;
     const BYTES: Self::Type = T::BYTES;
+    const VALUE: &str = unsafe {
+        str::from_utf8_unchecked(slice::from_raw_parts(
+            const {
+                &Cast::<T, N> {
+                    encoded: ManuallyDrop::new(T::BYTES),
+                }
+                .array
+            }
+            .as_ptr(),
+            N,
+        ))
+    };
+}
+
+union Cast<T: RetrieveString, const N: usize> {
+    encoded: ManuallyDrop<T::Type>,
+    array: [u8; N],
 }
 
 const TAG_CONT: u8 = 0b1000_0000;
